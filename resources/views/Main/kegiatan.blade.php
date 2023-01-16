35e48bfe32e01cbf4efa5a069a0f4f863cfafb83
HFM07 <VeramikaNZ@gmail.com>
2023-01-16 20:33:54 +0700

    modified:   resources/views/Main/home.blade.php 	modified:   resources/views/Main/kegiatan.blade.php 	modified:   resources/views/Main/kegiatanApproval.blade.php 	modified:   resources/views/Main/kegiatanDone.blade.php 	modified:   resources/views/Main/user.blade.php

--- resources/views/Main/kegiatan.blade.php
+++ resources/views/Main/kegiatan.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta charset="UTF-8">
@@ -100,4 +99,3 @@
     </script>
 </body>
 </html>
-@endsection
--- resources/views/Main/user.blade.php
+++ resources/views/Main/user.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta charset="UTF-8">
@@ -10,7 +9,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
-
 <body>
     <div class="container">
         <div class="alert alert-success text-center" role="alert">
@@ -96,4 +94,3 @@
     </script>
 </body>
 </html>
-@endsection
