3d7ad33e3952d56e39445be7f6c1aed0976d5359
HFM07 <VeramikaNZ@gmail.com>
2023-01-16 23:03:51 +0700

    modified:   public/css/Tampilan.css 	modified:   resources/views/Main/home.blade.php 	modified:   resources/views/Main/kegiatan.blade.php 	modified:   resources/views/Main/kegiatanApproval.blade.php 	modified:   resources/views/Main/kegiatanDone.blade.php 	modified:   resources/views/Main/user.blade.php

--- resources/views/Main/home.blade.php
+++ resources/views/Main/home.blade.php
@@ -17,28 +17,24 @@
                 <div class="Dropdown-content">
                     <a href="/Main/user">Daftar User</a>
                     <a href="/Tambah/TambahUser">Tambah User</a>
-                    <a href="/Edit/EditUser">Edit User</a>
                 </div>
             </li>
             <li class="Dropdown"><a href="javascript:void(0)" class="DropButton">Kegiatan</a>
                 <div class="Dropdown-content">
                     <a href="/Main/kegiatan">Daftar Kegiatan</a>
                     <a href="/Tambah/TambahKegiatan">Tambah Kegiatan</a>
-                    <a href="/Edit/EditKegiatan">Edit Kegiatan</a>
                 </div>
             </li>
             <li class="Dropdown"><a href="javascript:void(0)" class="DropButton">Kegiatan Approval</a>
                 <div class="Dropdown-content">
                     <a href="/Main/kegiatanApproval">Daftar Kegiatan Approval</a>
                     <a href="/Tambah/TambahKegiatanApproval">Tambah Kegiatan Approval</a>
-                    <a href="/Edit/EditKegiatanApproval">Edit Kegiatan Approval</a>
                 </div>
             </li>
             <li class="Dropdown"><a href="javascript:void(0)" class="DropButton">Kegiatan Done</a>
                 <div class="Dropdown-content">
                     <a href="/Main/kegiatanDone">Daftar Kegiatan Done</a>
                     <a href="/Tambah/TambahKegiatanDone">Tambah Kegiatan Done</a>
-                    <a href="/Edit/EditKegiatanDone">Edit Kegiatan Done</a>
                 </div>
             </li>
             <li><a href="">About Us</a></li>
