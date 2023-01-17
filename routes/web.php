14f552f9c2ca7f1ea8d0851314b8db753158c8aa
HFM07 <VeramikaNZ@gmail.com>
2023-01-17 16:50:03 +0700

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('Main/');
+    return view('Main/home');
 });
 
 Route::get('/register', [registerController::class, 'index'])->name('register');
