439a46341b7f6ab10c7561a470204ae9a014f1ab
HFM07 <VeramikaNZ@gmail.com>
2023-01-21 11:50:55 +0700

    modified:   resources/views/Tambah/TambahKegiatanApproval.blade.php 	modified:   resources/views/Tambah/TambahKegiatanDone.blade.php

--- resources/views/Tambah/TambahKegiatanDone.blade.php
+++ resources/views/Tambah/TambahKegiatanDone.blade.php
@@ -57,7 +57,7 @@
                 <label for="KegiatanId" class="form-label text-dark">ID Kegiatan</label>
                 <select name="KegiatanId" id="KegiatanId" class="Textbox text-dark">
                     @foreach ($kegiatan as $item => $i)
-                        <option value="{{ $i->Id }}">{{ $i->Id }} : {{$i->NamaKegiatan}}</option>
+                        <option value="{{ $i->Id }}">{{ $i->Id }} : {{ $i->NamaKegiatan }}</option>
                     @endforeach
                 </select>
             </div>
@@ -84,51 +84,4 @@
             <button type="submit" class="ButtonS" name="submit">Simpan</button>
         </form>
 </body>
-
 </html>
-=======
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Kegiatan</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    </head>
-
-    <body>
-        <h3>Tambah Kegiatan/h3>
-            <div class="my-3 p-3 bg-body rounded shadow-sm">
-                <a href="{{route('kegiatan')}}" class="btn btn-primary">Kembali</a>
-                <form action="{{ route('kegiatan.store') }}" method="POST">
-                    @csrf
-                    <input type="hidden" class="form-control" name='id'  id="id">
-                    <div class="mb-3 row">
-                        <label for="tgl_mulai" class="col-sm-2 col-form-label">Tanggal Mulai</label>
-                        <input type="date" class="form-control" name='tgl_mulai' id="tgl_mulai">
-                    </div>
-                </div>
-                    <div class="mb-3 row">
-                        <label for="tgl_selesai" class="col-sm-2 col-form-label">Tanggal Selesai</label>
-                        <input type="date" class="form-control" name='tgl_selesai' id="tgl_selesai">
-                    </div>
-                </div>
-                    <div class="mb-3 row">
-                        <label for="bukti_terlaksana" class="col-sm-2 col-form-label">Bukti Terlaksana</label>
-                        <input type="file" class="form-control" name="bukti_terlaksana" id="bukti_terlaksana">
-                    </div>
-                </div>
-                <div class="mb-3">
-                    <label for="kegiatan_id" class="form-label">ID Kegiatan</label>
-                    <select name="kegiatan_id" id="kegiatan_id" class="form-control">
-                        @foreach ($kegiatanApproval as $item => $i)
-                            <option value="{{ $i->id }}">{{ $i->kegiatan }}</option>
-                        @endforeach
-                    </select>
-            </div>
-                <button type="submit" class="btn btn-primary" name="submit">Simpan</button>
-            </div>
-        </form>
-    </body>
-</html>
->>>>>>> bc671aca241ff3f25d2b71fd3c74266d8f5fc0a9
