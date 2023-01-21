5a111961ce1e1c4f9ae553a7818ac3e4c14b4a73
HFM07 <VeramikaNZ@gmail.com>
2023-01-21 19:24:52 +0700

    modified:   public/css/Tampilan.css 	modified:   resources/views/AboutUs.blade.php 	modified:   resources/views/Edit/EditKegiatan.blade.php 	modified:   resources/views/Edit/EditKegiatanApproval.blade.php 	modified:   resources/views/Edit/EditKegiatanDone.blade.php 	modified:   resources/views/Edit/EditUser.blade.php 	modified:   resources/views/Main/home.blade.php 	modified:   resources/views/Main/kegiatan.blade.php 	modified:   resources/views/Main/kegiatanApproval.blade.php 	modified:   resources/views/Main/kegiatanDone.blade.php 	modified:   resources/views/Main/user.blade.php 	modified:   resources/views/Tambah/TambahKegiatan.blade.php 	modified:   resources/views/Tambah/TambahKegiatanApproval.blade.php 	modified:   resources/views/Tambah/TambahKegiatanDone.blade.php 	modified:   resources/views/Tambah/TambahUser.blade.php

--- resources/views/Main/kegiatanApproval.blade.php
+++ resources/views/Main/kegiatanApproval.blade.php
@@ -5,41 +5,43 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Kegiatan</title>
+    <title>Kegiatan Approval</title>
     <link rel="stylesheet" href="{{ asset('css/Tampilan.css') }}">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/js/all.min.js" integrity="sha512-rpLlll167T5LJHwp0waJCh3ZRf7pO6IT1+LZOhAyP6phAirwchClbTZV3iqL3BMrVxIYRbzGTpli4rfxsCK6Vw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
     <div class="Menu">
         <ul>
-            <li><a href="/">Home</a></li>
-            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton">User</a>
+            <li><i class="fa-solid fa-house"></i><a href="/">Home</a></li>
+            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton"><i class="fa-solid fa-user"></i>User</a>
                 <div class="Dropdown-content">
                     <a href="/Main/user">Daftar User</a>
                     <a href="/Tambah/TambahUser">Tambah User</a>
                 </div>
             </li>
-            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton">Kegiatan</a>
+            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton"><i class="fa-regular fa-calendar-plus"></i>Kegiatan</a>
                 <div class="Dropdown-content">
                     <a href="/Main/kegiatan">Daftar Kegiatan</a>
                     <a href="/Tambah/TambahKegiatan">Tambah Kegiatan</a>
                 </div>
             </li>
-            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton">Kegiatan Approval</a>
+            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton"><i class="fa-regular fa-calendar-check"></i>Kegiatan Approval</a>
                 <div class="Dropdown-content">
                     <a href="/Main/kegiatanApproval">Daftar Kegiatan Approval</a>
                     <a href="/Tambah/TambahKegiatanApproval">Tambah Kegiatan Approval</a>
                 </div>
             </li>
-            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton">Kegiatan Done</a>
+            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton"><i class="fa-solid fa-calendar-week"></i>Kegiatan Done</a>
                 <div class="Dropdown-content">
                     <a href="/Main/kegiatanDone">Daftar Kegiatan Done</a>
                     <a href="/Tambah/TambahKegiatanDone">Tambah Kegiatan Done</a>
                 </div>
             </li>
-            <li><a href="">About Us</a></li>
-            <li><a href="">Log Out</a></li>
+            <li><a href="/AboutUs"><i class="fa-solid fa-users"></i>About Us</a></li>
+            <li><a href=""><i class="fa-solid fa-arrow-right-from-bracket"></i>Log Out</a></li>
         </ul>
     </div>
     <div class="container">
