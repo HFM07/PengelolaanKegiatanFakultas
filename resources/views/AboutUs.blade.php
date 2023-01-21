e0a130dfa2643548482e83f40f02d90bd79ebecd
HFM07 <VeramikaNZ@gmail.com>
2023-01-21 11:54:11 +0700

    new file:   resources/views/AboutUs.blade.php

---
+++ resources/views/AboutUs.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="Menu">
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton">User</a>
+                <div class="Dropdown-content">
+                    <a href="/Main/user">Daftar User</a>
+                    <a href="/Tambah/TambahUser">Tambah User</a>
+                </div>
+            </li>
+            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton">Kegiatan</a>
+                <div class="Dropdown-content">
+                    <a href="/Main/kegiatan">Daftar Kegiatan</a>
+                    <a href="/Tambah/TambahKegiatan">Tambah Kegiatan</a>
+                </div>
+            </li>
+            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton">Kegiatan Approval</a>
+                <div class="Dropdown-content">
+                    <a href="/Main/kegiatanApproval">Daftar Kegiatan Approval</a>
+                    <a href="/Tambah/TambahKegiatanApproval">Tambah Kegiatan Approval</a>
+                </div>
+            </li>
+            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton">Kegiatan Done</a>
+                <div class="Dropdown-content">
+                    <a href="/Main/kegiatanDone">Daftar Kegiatan Done</a>
+                    <a href="/Tambah/TambahKegiatanDone">Tambah Kegiatan Done</a>
+                </div>
+            </li>
+            <li><a href="">About Us</a></li>
+            <li><a href="">Log Out</a></li>
+        </ul>
+    </div>
+</body>
+</html>
