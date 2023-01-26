59c81d04452c03dacbf3a80a4b608c9ff191a5b7
Dirham Triyadi <dirhamtriyadi@gmail.com>
2023-01-26 13:00:33 +0700

    add message error not allowed

--- resources/views/Main/kegiatanApproval.blade.php
+++ resources/views/Main/kegiatanApproval.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta charset="UTF-8">
@@ -8,45 +9,57 @@
     <title>Kegiatan Approval</title>
     <link rel="stylesheet" href="{{ asset('css/Tampilan.css') }}">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/js/all.min.js"
-        integrity="sha512-rpLlll167T5LJHwp0waJCh3ZRf7pO6IT1+LZOhAyP6phAirwchClbTZV3iqL3BMrVxIYRbzGTpli4rfxsCK6Vw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        integrity="sha512-rpLlll167T5LJHwp0waJCh3ZRf7pO6IT1+LZOhAyP6phAirwchClbTZV3iqL3BMrVxIYRbzGTpli4rfxsCK6Vw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
-        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
+
 <body>
     <div class="Menu">
         <ul>
             <li><i class="fa-solid fa-house"></i><a href="/Main/home">Home</a></li>
-            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton"><i class="fa-solid fa-user"></i>User</a>
+            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton"><i
+                        class="fa-solid fa-user"></i>User</a>
                 <div class="Dropdown-content">
                     <a href="/Main/user">Daftar User</a>
                     <a href="/Tambah/TambahUser">Tambah User</a>
                 </div>
             </li>
-            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton"><i class="fa-regular fa-calendar-plus"></i>Kegiatan</a>
+            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton"><i
+                        class="fa-regular fa-calendar-plus"></i>Kegiatan</a>
                 <div class="Dropdown-content">
                     <a href="/Main/kegiatan">Daftar Kegiatan</a>
                     <a href="/Tambah/TambahKegiatan">Tambah Kegiatan</a>
                 </div>
             </li>
-            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton"><i class="fa-regular fa-calendar-check"></i>Kegiatan Approval</a>
+            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton"><i
+                        class="fa-regular fa-calendar-check"></i>Kegiatan Approval</a>
                 <div class="Dropdown-content">
                     <a href="/Main/kegiatanApproval">Daftar Kegiatan Approval</a>
                     <a href="/Tambah/TambahKegiatanApproval">Tambah Kegiatan Approval</a>
                 </div>
             </li>
-            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton"><i class="fa-solid fa-calendar-week"></i>Kegiatan Done</a>
+            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton"><i
+                        class="fa-solid fa-calendar-week"></i>Kegiatan Done</a>
                 <div class="Dropdown-content">
                     <a href="/Main/kegiatanDone">Daftar Kegiatan Done</a>
                     <a href="/Tambah/TambahKegiatanDone">Tambah Kegiatan Done</a>
                 </div>
             </li>
             <li><a href="/AboutUs"><i class="fa-solid fa-users"></i>About Us</a></li>
-            <li><a href="{{ route('logout')}}"><i class="fa-solid fa-arrow-right-from-bracket"></i>Log Out</a></li>
+            <li><a href="{{ route('logout') }}"><i class="fa-solid fa-arrow-right-from-bracket"></i>Log Out</a></li>
         </ul>
     </div>
     <div class="container">
+        @if (session('loginError'))
+            <div class="alert alert-danger">
+                {{ session('loginError') }}
+            </div>
+        @endif
         <table class="table table-hover table-striped table-bordered">
             <thead class="Thead">
                 <tr>
@@ -63,14 +76,14 @@
                     <tr>
                         <td>{{ $item + 1 }}</td>
                         <td>{{ $i->Id }}</td>
-                        <td>{{ $i->user->id }} : {{$i->user->username}}</td>
-                        <td>{{ $i->kegiatan->Id }} : {{$i->kegiatan->NamaKegiatan}}</td>
+                        <td>{{ $i->user->id }} : {{ $i->user->username }}</td>
+                        <td>{{ $i->kegiatan->Id }} : {{ $i->kegiatan->NamaKegiatan }}</td>
                         <td>{{ $i->Status }}</td>
                         <td>
-                            <a href="/kegiatanApproval/{{ $i->Id }}/getEditKegiatanApproval"
-                                 class="ButtonS"><i class="fa-regular fa-pen-to-square"></i>Edit</a>
+                            <a href="/kegiatanApproval/{{ $i->Id }}/getEditKegiatanApproval" class="ButtonS"><i
+                                    class="fa-regular fa-pen-to-square"></i>Edit</a>
                             <a href="javascript:void(0)" class="ButtonH" data-id="{{ $i->Id }}"><i
-                                class="fa-solid fa-trash"></i>Hapus</a>
+                                    class="fa-solid fa-trash"></i>Hapus</a>
                         </td>
                     </tr>
                 @endforeach
@@ -114,9 +127,9 @@
                         $.ajax({
                             type: "DELETE",
                             url: "/kegiatanApproval/" + $(this).data('id') +
-                            "/hapusKegiatanApproval",
+                                "/hapusKegiatanApproval",
                             data: {
-                                _token: "{{csrf_token()}}"
+                                _token: "{{ csrf_token() }}"
                             },
                             success: function(response) {
                                 location.reload();
@@ -128,4 +141,5 @@
         });
     </script>
 </body>
+
 </html>
--- resources/views/Main/user.blade.php
+++ resources/views/Main/user.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta charset="UTF-8">
@@ -8,45 +9,57 @@
     <title>User</title>
     <link rel="stylesheet" href="{{ asset('css/Tampilan.css') }}">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/js/all.min.js"
-        integrity="sha512-rpLlll167T5LJHwp0waJCh3ZRf7pO6IT1+LZOhAyP6phAirwchClbTZV3iqL3BMrVxIYRbzGTpli4rfxsCK6Vw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        integrity="sha512-rpLlll167T5LJHwp0waJCh3ZRf7pO6IT1+LZOhAyP6phAirwchClbTZV3iqL3BMrVxIYRbzGTpli4rfxsCK6Vw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
-        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
+
 <body>
     <div class="Menu">
         <ul>
             <li><i class="fa-solid fa-house"></i><a href="/Main/home">Home</a></li>
-            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton"><i class="fa-solid fa-user"></i>User</a>
+            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton"><i
+                        class="fa-solid fa-user"></i>User</a>
                 <div class="Dropdown-content">
                     <a href="/Main/user">Daftar User</a>
                     <a href="/Tambah/TambahUser">Tambah User</a>
                 </div>
             </li>
-            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton"><i class="fa-regular fa-calendar-plus"></i>Kegiatan</a>
+            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton"><i
+                        class="fa-regular fa-calendar-plus"></i>Kegiatan</a>
                 <div class="Dropdown-content">
                     <a href="/Main/kegiatan">Daftar Kegiatan</a>
                     <a href="/Tambah/TambahKegiatan">Tambah Kegiatan</a>
                 </div>
             </li>
-            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton"><i class="fa-regular fa-calendar-check"></i>Kegiatan Approval</a>
+            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton"><i
+                        class="fa-regular fa-calendar-check"></i>Kegiatan Approval</a>
                 <div class="Dropdown-content">
                     <a href="/Main/kegiatanApproval">Daftar Kegiatan Approval</a>
                     <a href="/Tambah/TambahKegiatanApproval">Tambah Kegiatan Approval</a>
                 </div>
             </li>
-            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton"><i class="fa-solid fa-calendar-week"></i>Kegiatan Done</a>
+            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton"><i
+                        class="fa-solid fa-calendar-week"></i>Kegiatan Done</a>
                 <div class="Dropdown-content">
                     <a href="/Main/kegiatanDone">Daftar Kegiatan Done</a>
                     <a href="/Tambah/TambahKegiatanDone">Tambah Kegiatan Done</a>
                 </div>
             </li>
             <li><a href="/AboutUs"><i class="fa-solid fa-users"></i>About Us</a></li>
-            <li><a href="{{ route('logout')}}"><i class="fa-solid fa-arrow-right-from-bracket"></i>Log Out</a></li>
+            <li><a href="{{ route('logout') }}"><i class="fa-solid fa-arrow-right-from-bracket"></i>Log Out</a></li>
         </ul>
     </div>
     <div class="container">
+        @if (session('loginError'))
+            <div class="alert alert-danger">
+                {{ session('loginError') }}
+            </div>
+        @endif
         <table class="table table-hover table-striped table-bordered">
             <thead class="Thead">
                 <tr>
@@ -69,10 +82,10 @@
                         <td>{{ $i->password }}</td>
                         <td>{{ $i->hakAkses }}</td>
                         <td>
-                            <a href="/user/{{ $i->id }}/getEditUser"
-                                class="ButtonS"><i class="fa-regular fa-pen-to-square"></i>Edit</a>
+                            <a href="/user/{{ $i->id }}/getEditUser" class="ButtonS"><i
+                                    class="fa-regular fa-pen-to-square"></i>Edit</a>
                             <a href="javascript:void(0)" class="ButtonH" data-id="{{ $i->id }}"><i
-                                class="fa-solid fa-trash"></i>Hapus</a>
+                                    class="fa-solid fa-trash"></i>Hapus</a>
                         </td>
                     </tr>
                 @endforeach
@@ -117,9 +130,9 @@
                             type: "DELETE",
                             url: "/user/" + $(this).data('id') +
                                 "/hapusUser",
-                                data: {
-                                    _token: "{{csrf_token()}}"
-                                },
+                            data: {
+                                _token: "{{ csrf_token() }}"
+                            },
                             success: function(response) {
                                 location.reload();
                             }
@@ -130,4 +143,5 @@
         });
     </script>
 </body>
+
 </html>
