9978ab6f3563e1002326934ef9a85d830a34e859
HFM07 <VeramikaNZ@gmail.com>
2023-01-26 13:04:19 +0700

    modified:   app/Http/Middleware/CheckHakAkses.php 	modified:   resources/views/Main/home.blade.php 	modified:   resources/views/Main/kegiatan.blade.php 	modified:   resources/views/Main/kegiatanApproval.blade.php 	modified:   resources/views/Main/kegiatanDone.blade.php 	modified:   resources/views/Main/user.blade.php

--- resources/views/Main/home.blade.php
+++ resources/views/Main/home.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,45 +8,57 @@
     <title>Home</title>
     <link rel="stylesheet" href="{{ asset('css/Tampilan.css') }}">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/js/all.min.js"
-        integrity="sha512-rpLlll167T5LJHwp0waJCh3ZRf7pO6IT1+LZOhAyP6phAirwchClbTZV3iqL3BMrVxIYRbzGTpli4rfxsCK6Vw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        integrity="sha512-rpLlll167T5LJHwp0waJCh3ZRf7pO6IT1+LZOhAyP6phAirwchClbTZV3iqL3BMrVxIYRbzGTpli4rfxsCK6Vw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
-        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
+
 <body>
     <div class="Menu">
         <ul>
             <li><i class="fa-solid fa-house"></i><a href="/Main/home">Home</a></li>
-            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton"><i class="fa-solid fa-user"></i>User</a>
+            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton"><i
+                        class="fa-solid fa-user"></i>User</a>
                 <div class="Dropdown-content">
                     <a href="/Main/user">Daftar User</a>
                     <a href="/Tambah/TambahUser">Tambah User</a>
                 </div>
             </li>
-            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton"><i class="fa-regular fa-calendar-plus"></i>Kegiatan</a>
+            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton"><i
+                        class="fa-regular fa-calendar-plus"></i>Kegiatan</a>
                 <div class="Dropdown-content">
                     <a href="/Main/kegiatan">Daftar Kegiatan</a>
                     <a href="/Tambah/TambahKegiatan">Tambah Kegiatan</a>
                 </div>
             </li>
-            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton"><i class="fa-regular fa-calendar-check"></i>Kegiatan Approval</a>
+            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton"><i
+                        class="fa-regular fa-calendar-check"></i>Kegiatan Approval</a>
                 <div class="Dropdown-content">
                     <a href="/Main/kegiatanApproval">Daftar Kegiatan Approval</a>
                     <a href="/Tambah/TambahKegiatanApproval">Tambah Kegiatan Approval</a>
                 </div>
             </li>
-            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton"><i class="fa-solid fa-calendar-week"></i>Kegiatan Done</a>
+            <li class="Dropdown"><a href="javascript:void(0)" class="DropButton"><i
+                        class="fa-solid fa-calendar-week"></i>Kegiatan Done</a>
                 <div class="Dropdown-content">
                     <a href="/Main/kegiatanDone">Daftar Kegiatan Done</a>
                     <a href="/Tambah/TambahKegiatanDone">Tambah Kegiatan Done</a>
                 </div>
             </li>
-            <li><a href="{{ route('logout')}}"><i class="fa-solid fa-arrow-right-from-bracket"></i>Log Out</a></li>
+            <li><a href="{{ route('logout') }}"><i class="fa-solid fa-arrow-right-from-bracket"></i>Log Out</a></li>
         </ul>
     </div>
     <br>
     <div class="container">
+        @if (session('loginError'))
+            <div class="alert alert-danger">
+                {{ session('loginError') }}
+            </div>
+        @endif
         <div class="Box">
             <h5>Detail Jobdesk:</h5>
             <ul>
@@ -66,4 +79,5 @@
         </div>
     </div>
 </body>
+
 </html>
